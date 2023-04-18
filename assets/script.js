4d6cfa50972b4cdfe1d69ef1226d5b80dfaf0315
dsoda86 <122141740+dsoda86@users.noreply.github.com>
2023-04-18 13:08:29 -0700

    getPromts() function attempt with conditional statements for criteria

--- assets/script.js
+++ assets/script.js
@@ -17,13 +17,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// password user choice length: 8-128 characters
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
+
+
+// password user choice length is >= 8 <= 128 characters
+var characterLength = [];
 // possible user choices: abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+`~[]{}|;:',<>./?
 var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numberOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var specialCharOptions = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "`", "~", "[", "]", "{", "}", "|", ";", ":", "'", ",", "<", ">", ".", "/", "?"]
-// something similar to this exaple from activites probably needed: var index = Math.floor(Math.random() * options.length); 
+var userChoice = [];
+
+// var index = Math.floor(Math.random() * options.length);
+// something similar to this example from activites probably needed in a function somewhere
  
 // Write password to the #password input
 function writePassword() {
@@ -34,6 +43,38 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// When button clicked, console says generatePassword() is not defined so we need to define this function
+// Function would be defined by prompt criteria chosen
+function generatePassword() {
 
-generateBtn.addEventListener("click", writePassword);
+}
+// getPrompts() returning undefined in console after selecting a number within range instead of going through rest of prompts?
+function getPrompts() {
+  characterLength = prompt("How long do you want your password to be? (8-128 characters)")
+  
+  if (characterLength < 8 || characterLength > 128 ) {
+    alert("Character length needs to be a number, 8 - 128.");
+    return false;
+  }
+
+  if (confirm("Do you want your password to include lowercase letters?")) {
+    userChoice = userChoice.concat(lowerCaseOptions);
+    //if not then do not concat arrays / something needs to happen?
+  }
+
+  if (confirm("Do you want your password to include uppercase letters?")) {
+    userChoice = userChoice.concat(upperCaseOptions);
+    //if not then do not concat arrays / something needs to happen?
+  }
+
+  if (confirm("Do you want your password to include numbers?")) {
+    userChoice = userChoice.concat(numberOptions);
+    //if not then do not concat arrays / something needs to happen?
+  }
+
+  if (confirm("Do you want your password to include special characters?")) {
+    userChoice = userChoice.concat(speacialCharOptions);
+    //if not then do not concat arrays / something needs to happen?
+  }
+  return true;
+}
